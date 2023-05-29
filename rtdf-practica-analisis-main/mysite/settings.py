1b7db869081bddcad499db4f8db00342b5c9628a
Blap00 <fa.palma@duocuc.cl>
2023-05-29 12:42:29 -0400

    disable local Database

--- rtdf-practica-analisis-main/mysite/settings.py
+++ rtdf-practica-analisis-main/mysite/settings.py
@@ -91,17 +91,17 @@ DATABASES = {
     }
 }
 ##LOCAL DATABASE
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'practica',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'practica',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+#     }
+# }
 ##CLOUD DATABASE
 DATABASES = {
     'default': {
